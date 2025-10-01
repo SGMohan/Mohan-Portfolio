2cc8cd11105782d5f746396f93d92287b10a6281
SGMohan <iamsgmohan@gmail.com>
2025-10-01 16:52:16 +0530

    Portfolio Done

---
+++ script.js
@@ -0,0 +1,32 @@
+const sideMenu = document.getElementById("sideMenu");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
+// Mobile menu toggle functions
+function openMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
+}
+function closeMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
+}
+
+// NavBar Scroll Effect
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navBar.classList.add(
+      "bg-transparent",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.remove("bg-white/60", "bg-opacity-50", "shadow-sm");
+  } else {
+    navBar.classList.remove(
+      "bg-transparent",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm"
+    );
+    navLinks.classList.add("bg-white/60", "bg-opacity-50", "shadow-sm");
+  }
+});
